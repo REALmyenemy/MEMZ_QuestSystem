17d1a4fed4597c43b3cfcb9099d302db7f23b96a
REALmyenemy <26635506+REALmyenemy@users.noreply.github.com>
2021-01-08 23:46:22 +0100

    Backend finished
    Starting command calls and parameters
    Still need to add to imported and a GUI, plus load at new game.

--- js/plugins/ME_QuestSystem.js
+++ js/plugins/ME_QuestSystem.js
@@ -1,7 +1,7 @@
 /*:
- * Version 1.0.0
+ * Version 0.0.2
  * @target MZ
- * Last update 02/01/21
+ * Last update 08/01/21
  * @author myenemy
  * @plugindesc A plugin to introduce a quest system
  * 
@@ -46,6 +46,8 @@
 
 var ME_QuestList;
 
+Game_Player.prototype.ME_QS_PlayerList=[];
+
 DataManager.ME_QS_loadQuests = function(file)
 {
 	this.loadDataFile("ME_QuestList",file)
@@ -61,17 +63,206 @@ ME_Quest.prototype.constructor = ME_Quest;
 
 ME_Quest.prototype.initialize = function()
 {
-    this.setup(questId);
+    this.setup();
 };
 
-ME_Quest.prototype.setup = function(questId) {
-    this._id=0;
-	this._name="";
-	this._type="";
-	this._status=0;
-	this._text="";
-	this._goals=[];
-	this._rewards=[];
-	this._nextQuest=0;
+ME_Quest.prototype.setup = function() {
+    this.id=0;
+	this.name="";
+	this.type="";
+	this.status=0;
+	this.text="";
+	this.goals=[];
+	this.rewards={};
+	this.nextQuest=0;
 };
+
+var ME_QuestSystem = function()
+{};
+
+ME_QuestSystem.prototype.checkQuestExists = function (questId)
+{
+	if (!ME_QuestList)
+		return false;
+	for (var i=1;i<ME_QuestList.length;i++)
+	{
+		if (ME_QuestList[i].id==questId)
+		{
+			return i;
+		}
+	}
+	return false;
+};
+
+
+ME_QuestSystem.prototype.checkQuestOwned = function (questId)
+{
+	if (!$gamePlayer.ME_QS_PlayerList)
+		return false;
+	for (var i=0;i<$gamePlayer.ME_QS_PlayerList.length;i++)
+	{
+		if ($gamePlayer.ME_QS_PlayerList[i].id==questId)
+		{
+			return i;
+		}
+	}
+	return false;
+};
+
+ME_QuestSystem.prototype.giveQuest = function (questId)
+{
+	var quest = this.checkQuestExists(questId);
+	if (quest)
+	{
+		if (!this.checkQuestOwned(questId))
+		{
+			var actualQuest=ME_QuestList[quest];
+			actualQuest.status=1;
+			$gamePlayer.ME_QS_PlayerList.push(actualQuest);
+			if (actualQuest.startScript)
+				eval(actualQuest.startScript);
+
+			return true;
+		}
+	}
+	return false;
+};
+
+ME_QuestSystem.prototype.removeQuest = function (questId)
+{
+	var quest = this.checkQuestExists(questId);
+	if (quest||quest==0)
+	{
+		var index=this.checkQuestOwned(questId);
+		console.log(index)
+		if (index)
+		{
+			$gamePlayer.ME_QS_PlayerList.splice(index, 1);
+			return true;
+		}
+	}
+	return false;
+};
+
+ME_QuestSystem.prototype.awardQuest = function (questId)
+{
+	var quest = this.checkQuestExists(questId);
+	if (quest)
+	{
+		var index=this.checkQuestOwned(questId);
+		if (index||index==0)
+		{
+			var actualQuest=$gamePlayer.ME_QS_PlayerList[index];
+			var rewards = actualQuest.rewards;
+			this.award(rewards);
+			if (actualQuest.nextQuest&&actualQuest.nextQuest>0)
+			{
+				this.giveQuest(actualQuest.nextQuest);
+			}
+			actualQuest.status=2;
+			this.removeQuest(questId);
+			
+			return true;
+		}
+	}
+	return false;
+};
+
+ME_QuestSystem.prototype.award= function(rewardList)
+{
+	if (rewardList.gold)
+	{
+		$gameParty.gainGold(rewardList.gold);
+	}
+	if (rewardList.partyexp)
+	{
+		var interpreter = new Game_Interpreter();
+		interpreter.command315([0,0,0,0,rewardList.partyexp,false]);
+	}
+	if (rewardList.leaderexp)
+	{
+		$gameParty.leader().changeExp($gameParty.leader().currentExp() + rewardList.leaderexp, false);
+	}
+	if (rewardList.items)
+	{
+		var items = rewardList.items;
+		for (var i =0;i<items.length;i++)
+			this.giveItem(items[i])
+
+	}
+	if (rewardList.runScript)
+	{
+		eval(rewardList.runScript);
+	}
+};
+
+ME_QuestSystem.prototype.giveItem = function(item)
+{
+	switch(item.type)
+	{
+		case "item":
+			$gameParty.gainItem($dataItems[item.id],item.amount);
+			break;
+		case "weapon":
+			$gameParty.gainItem($dataWeapons[item.id],item.amount,true);
+			break;
+		case "armor":
+			$gameParty.gainItem($dataArmors[item.id],item.amount,true);
+			break;
+	}	
+}
+
+ME_QuestSystem.prototype.checkFinished = function(goals)
+{
+	var finished=true;
+	for (var i=0;finished&&i<goals.length;i++)
+	{
+		if (!this.checkGoal(goals[i]))
+		finished=false;
+	}
+	return finished;
+};
+
+ME_QuestSystem.prototype.checkGoal = function(goal)
+{
+	if (goal.checkScript)
+	{
+		return eval(goal.checkScript);
+	}
+	else if (goal.varCount&&goal.varMax)
+	{
+		return $gameVariables.value(goal.varCount)==$gameVariables.value(goal.varMax);
+	}
+	else if (goal.switches)
+	{
+		if (Array.isArray(goal.switches))
+		{
+			var isTrue=true;
+			for (var i=0;isTrue && i<goal.switches.length; i++)
+				if (!$gameSwitches.value(goal.switches[i]))
+					isTrue=false;
+
+			return isTrue;
+		}
+		else
+			return $gameSwitches.value(goal.switches)
+	}
+	return true; //Since it's player controlled
+};
+
+ME_QuestSystem.prototype.completeQuest = function(questId)
+{
+	var index=this.checkQuestOwned(questId);
+	if (index||index==0)
+	{
+		var actualQuest=$gamePlayer.ME_QS_PlayerList[index];
+		
+		if (this.checkFinished(actualQuest.goals))
+		{
+			return this.awardQuest(questId);
+		}
+	}
+	return false;	
+};
+
 
